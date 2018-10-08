828c282be9735e670c8584f4dc82d2e2325b9a12
HP-ROGER\rhumf <rhumfloripa@gmail.com>
2018-10-08 19:17:12 -0300

    add projeto ao github

--- README.md
+++ README.md
@@ -1,2 +1,14 @@
-# mws-restaurant-stage-1
-mws-restaurant-stage-1
+# Mobile Web Specialist Certification Course
+---
+#### _Three Stage Course Material Project - Restaurant Reviews_
+
+## Project Overview: Stage 1
+
+Para executar o projeto utilize:
+
+Inicialize um servidor cliente local usando Python no seu terminal:
+Python 2: python -m SimpleHTTPServer 8000
+Python 3: python3 -m http.server 8000
+Acesse o site no seu navegador em http://localhost:8000
+
+
---
+++ sw.js
@@ -0,0 +1,47 @@
+var nomeDoCache = 'restaurant-cache';
+var arquivosParaCache = [
+    'index.html',
+    'restaurant.html',
+    'css/styles.css',
+    'js/dbhelper.js',
+    'js/main.js',
+    'js/restaurant_info.js',
+    'data/restaurants.json',
+    'img/1.jpg',
+    'img/2.jpg',
+    'img/3.jpg',
+    'img/4.jpg',
+    'img/5.jpg',
+    'img/6.jpg',
+    'img/7.jpg',
+    'img/8.jpg',
+    'img/9.jpg',
+    'img/10.jpg',
+];
+// Perform install steps
+self.addEventListener('install', function (event) {
+    console.log('sw instalado');
+    event.waitUntil(
+        caches.open(nomeDoCache)
+            .then(function (cache) {
+                console.log('cache iniciado');
+                return cache.addAll(arquivosParaCache);
+            })
+    );
+});
+self.addEventListener('activate',  event => {
+    console.log('ativo');
+    event.waitUntil(self.clients.claim());
+});
+
+self.addEventListener('fetch', event => {
+    console.log('fetch');
+    event.respondWith(
+        caches.match(event.request, { ignoreSearch: true }).then(response => {
+            if (response) return response;
+            return fetch(event.request);
+        }).catch((error) => {
+            console.log(error);}
+        )
+    );
+});
